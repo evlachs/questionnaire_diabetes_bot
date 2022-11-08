0e003385dae3056fb65a79ed969fdb9f94874012
evlachs <evlach_e@mail.ru>
2022-11-08 14:12:03 +0300

    added processing of messages with photos

--- handlers/message_handlers/message_filter_handler.py
+++ handlers/message_handlers/message_filter_handler.py
@@ -15,16 +15,23 @@ async def delete_message(message: types.Message, time: int):
     await bot.delete_message(message.chat.id, message.message_id)
 
 
-@dp.message_handler()
+@dp.message_handler(content_types=['text', 'photo'])
 async def filter_messages(message: types.Message):
     current_chat_id = message.chat.id
+    print(message)
     if current_chat_id != GROUP_ID:
         return
     sm = SheetManager(gc, SPREADSHEET_ID)
     sm.add_new_message()
     message_content = set()
     bad_words_content = set(json.load(open('data/bad_words.json')))
-    for word in message.text.split():
+    if message.content_type == 'photo':
+        text = message.caption
+    else:
+        text = message.text
+    if not text:
+        return
+    for word in text.split():
         translated_word = word.lower().translate(str.maketrans('', '', string.punctuation))
         message_content.add(translated_word)
     if message_content.intersection(bad_words_content):
