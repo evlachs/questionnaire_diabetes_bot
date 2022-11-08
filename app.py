477ef5838e815bb30e59982c457f21b36cb2ebd0
evlachs <evlach_e@mail.ru>
2022-11-08 17:35:55 +0300

    the launch of the bot has been rewritten on webhooks

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from aiogram import executor
 
 from loader import dp
 from utils import set_default_commands
+from conf import WEBHOOK_URL_PATH, WEBHOOK_URL_BASE, WEBHOOK_SSL_CERT, WEBHOOK_PORT, WEBHOOK_LISTEN
 
 import handlers
 
@@ -9,7 +10,20 @@ import handlers
 async def on_startup(dispatcher):
     """Sets default commands for the bot, initializes the client, launches the loop with the invite_users function"""
     await set_default_commands(dispatcher)
+    await dp.bot.delete_webhook()
+    await dp.bot.set_webhook(
+        url=f'{WEBHOOK_URL_BASE}{WEBHOOK_URL_PATH}',
+        certificate=open(WEBHOOK_SSL_CERT, 'r'),
+        drop_pending_updates=True
+    )
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dp,
+        webhook_path=WEBHOOK_URL_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT
+    )
--- conf/config.py
+++ conf/config.py
@@ -10,3 +10,13 @@ BOT_URL = 'URL'
 GROUP_ID = 'GROUP_ID'
 
 SERVICE_ACCOUNT = 'path/to/service_account_file.json'  # it's recommended to place it in the 'data' folder
+
+WEBHOOK_HOST = 'IP-address of your sever'
+WEBHOOK_PORT = 443  # 443, 80, 88 or 8443
+WEBHOOK_LISTEN = 'IP-address of your sever'  # or 0.0.0.0
+
+WEBHOOK_SSL_CERT = 'path/to/webhook_cert.pem'  # it's recommended to place it in the 'data' folder
+WEBHOOK_SSL_PRIVATE = 'path/to/webhook_pkey.pem'  # it's recommended to place it in the 'data' folder
+
+WEBHOOK_URL_BASE = f'https://{WEBHOOK_HOST}:{WEBHOOK_PORT}'
+WEBHOOK_URL_PATH = f'/webhook/{BOT_TOKEN}'
