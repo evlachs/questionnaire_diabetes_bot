56f668891c65a7d6bec4054eeb61ba1c80e81a55
evlachs <evlach_e@mail.ru>
2022-11-10 14:09:35 +0300

    added aiohttp server

--- web_server/web_app/forum/routes.py
+++ web_server/web_app/forum/routes.py
@@ -6,7 +6,7 @@ from conf import BOT_TOKEN
 
 
 async def handle(request):
-    print(request, '\n', request.match_info.get('PlainResource'))
+    print(request, '\n', request.json())
     if request.match_info.get('token') == BOT_TOKEN:
         print('HGASGKGKASJGDJKHGASJKHGDKJHGA')
         request_body_dict = await request.json()
