6c7f50aea7b20eca3d1d90f95b88acf8d8df76f7
evlachs <evlach_e@mail.ru>
2022-11-12 01:44:39 +0300

    new update

--- web_server/web_app/forum/routes.py
+++ web_server/web_app/forum/routes.py
@@ -1,5 +1,3 @@
-import json
-
 from web_server.web_app.forum import views
 from aiohttp import web
 from loader import dp
@@ -10,10 +8,8 @@ from conf import BOT_TOKEN
 async def handle(request):
     if request.match_info.get_info()['path'].strip('/') == BOT_TOKEN:
         request_body_dict = await request.json()
-        print(request_body_dict)
         update = types.update.Update.to_object(request_body_dict)
-        print(update)
-        await dp.process_update(update)
+        await dp.process_updates(update)
         return web.Response()
     else:
         return web.Response(status=403)
