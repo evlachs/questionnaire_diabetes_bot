8bc0b304b81fd381fe7082f123dcdb8cf26c6c59
evlachs <evlach_e@mail.ru>
2022-11-11 10:08:30 +0300

    new update

--- web_server/web_app/forum/routes.py
+++ web_server/web_app/forum/routes.py
@@ -9,9 +9,9 @@ async def handle(request):
     if request.match_info.get_info()['path'].strip('/') == BOT_TOKEN:
         request_body_dict = await request.json()
         print(request_body_dict)
-        update = types.update.Update.to_object(request_body_dict)
+        update = types.update.Update(request_body_dict)
         print(update)
-        await dp.process_updates(types.Update(update))
+        await dp.process_update(update)
         return web.Response()
     else:
         return web.Response(status=403)
