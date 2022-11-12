0b03ee5bef647702df88d315f4164af74a64b011
evlachs <evlach_e@mail.ru>
2022-11-12 18:27:42 +0300

    new commit

--- web_server/web_app/forum/routes.py
+++ web_server/web_app/forum/routes.py
@@ -10,7 +10,7 @@ async def handle(request):
         request_body_dict = await request.json()
         print(request.match_info.get_info())
         print(request_body_dict)
-        update = types.base.TelegramObject.to_object(request_body_dict)
+        update = types.update.Update.to_object(request_body_dict)
         print(type(update), update)
         await dp.process_updates([update])
         return web.Response()
